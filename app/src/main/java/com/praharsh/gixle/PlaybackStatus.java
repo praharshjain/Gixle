ccdfb696fcf4085305dcd0e1aac148a377794828
Praharsh Jain <praharshsamria@gmail.com>
2017-05-17 23:34:55 +0530

    Added code

---
+++ app/src/main/java/com/praharsh/gixle/PlaybackStatus.java
@@ -0,0 +1,6 @@
+package com.praharsh.gixle;
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/com/praharsh/gixle/onItemClickListener.java
@@ -0,0 +1,7 @@
+package com.praharsh.gixle;
+
+import android.view.View;
+
+public interface onItemClickListener {
+    void onClick(View view, int index);
+}
